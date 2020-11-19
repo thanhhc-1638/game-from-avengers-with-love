218a0f7fa5119c0a5731195238f7eb8964d572f6
HaCongThanh69 <ha.cong.thanh@framgia.com>
2020-11-19 20:20:29 +0700

    Task#5: Check Deployer success

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    HCT
+                    HCT Ahihi
                 </div>
 
                 <div class="links">
